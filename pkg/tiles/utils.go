c2ece22da34d2cb06a102dc76e8d1b13d4ec0696
Luqqk <mikolajczak.luq@gmail.com>
2018-09-09 00:09:49 +0200

    Merge files, add ParseBaseURL function

--- pkg/tiles/utils.go
+++ pkg/tiles/utils.go
@@ -1,9 +1,133 @@
 package tiles
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 	"time"
 )
+
+// Options struct stores all available flags
+// and their values set by user.
+type Options struct {
+	URL     string
+	Layer   string
+	Format  string
+	Service string
+	Version string
+	Width   string
+	Height  string
+	Srs     string
+	Styles  string
+	Zooms   Zooms
+	Bbox    Bbox
+	// If all options are correct,
+	// build base URL for all tiles
+	// requests.
+	BaseURL string
+}
+
+// ValidateOptions validates options supplied by user.
+// Downloading will start only, if all required options
+// have been passed in correct format.
+func (options *Options) ValidateOptions() error {
+	switch {
+	case options.URL == "":
+		return errors.New("Wms server url is required")
+	case options.Layer == "":
+		return errors.New("Layer name is required")
+	case options.Zooms == nil:
+		return errors.New("Zooms are required")
+	case options.Bbox == Bbox{}:
+		return errors.New("Bbox is required")
+	default:
+		options.ParseBaseURL()
+		return nil
+	}
+}
+
+// ParseBaseURL builds base URL for all
+// tiles requests based on passed arguments.
+func (options *Options) ParseBaseURL() error {
+	u, err := url.Parse(options.URL)
+	if err != nil {
+		return err
+	}
+	if u.Scheme == "" {
+		u.Scheme = "https"
+	}
+	// Set query parameters.
+	q := u.Query()
+	q.Set("format", options.Format)
+	q.Set("service", options.Service)
+	q.Set("version", options.Version)
+	q.Set("request", "WMS")
+	q.Set("srs", options.Srs)
+	q.Set("width", options.Width)
+	q.Set("height", options.Height)
+	q.Set("layers", options.Layer)
+	q.Set("styles", options.Styles)
+	// Encode and set BaseURL field of
+	// the Options struct.
+	u.RawQuery = q.Encode()
+	options.BaseURL = u.String()
+	return nil
+}
+
+// Zooms stores zoom levels, for which
+// tiles should be downloaded.
+type Zooms []int
+
+// String is the method to format the flag's value, part of the flag.Value interface.
+// The String method's output will be used in diagnostics.
+func (zooms *Zooms) String() string {
+	return fmt.Sprint(*zooms)
+}
+
+// Set is the method to set the flag value, part of the flag.Value interface.
+// Converts comma-separated values (string in "int,int,int,(...)" format)
+// to Zooms type.
+func (zooms *Zooms) Set(value string) error {
+	for _, val := range strings.Split(value, ",") {
+		zoom, err := strconv.Atoi(val)
+		if err != nil {
+			return err
+		}
+		*zooms = append(*zooms, zoom)
+	}
+	return nil
+}
+
+// Bbox stores a web mercator bounding box, for which
+// tiles should be downloaded.
+type Bbox struct {
+	Left   float64
+	Bottom float64
+	Right  float64
+	Top    float64
+}
+
+// String is the method to format the flag's value, part of the flag.Value interface.
+// The String method's output will be used in diagnostics.
+func (bbox *Bbox) String() string {
+	return fmt.Sprint(*bbox)
+}
+
+// Set is the method to set the flag value, part of the flag.Value interface.
+// Converts comma-separated values (string in "left,bottom,right,top" format)
+// to Bbox struct.
+func (bbox *Bbox) Set(value string) error {
+	bboxSlice := strings.Split(value, ",")
+	left, _ := strconv.ParseFloat(bboxSlice[0], 64)
+	bottom, _ := strconv.ParseFloat(bboxSlice[1], 64)
+	right, _ := strconv.ParseFloat(bboxSlice[2], 64)
+	top, _ := strconv.ParseFloat(bboxSlice[3], 64)
+	*bbox = Bbox{Left: left, Bottom: bottom, Right: right, Top: top}
+	return nil
+}
 
 // Create a Client for control over HTTP client settings.
 // Client is safe for concurrent use by multiple goroutines
@@ -12,8 +136,8 @@ var client = &http.Client{
 	Timeout: time.Second * 30,
 }
 
-// GetTile sends http.Get request to WMS Server
+// Get sends http.Get request to WMS Server
 // and returns response content.
-func GetTile() {
+func Get() {
 	// To do
 }
