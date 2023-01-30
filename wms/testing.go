626e70e4a95919196ec11696228b768967ca0904
Lukasz Mikolajczak <mikolajczak.luq@gmail.com>
2023-01-30 21:22:45 +0100

    Add TestClientWithServer helper

---
+++ wms/testing.go
@@ -0,0 +1,23 @@
+package wms
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestClientWithServer(t *testing.T) (*Client, *http.ServeMux, func()) {
+	t.Helper()
+
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+
+	client, err := NewClient(server.URL)
+	if err != nil {
+		t.Fatalf("err = %v; want: nil", err)
+	}
+
+	return client, mux, func() {
+		server.Close()
+	}
+}
